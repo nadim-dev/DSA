2d5068398c95097feb7e18bad68fedd48a2e95eb
Nadim Khan <khanm99098@gmail.com>
2026-06-24 13:58:09 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (59.58%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,14 +1,35 @@
 class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
-        startPos=-1
-        endPos=-1
-        for i in range(len(nums)):
-            if nums[i] == target:
-                if startPos == -1:
-                    startPos=i
-                endPos=i
+    def lowerbound(self,nums,target):
+        lb=-1
+        low=0
+        high=len(nums)-1
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]>=target:
+                lb=mid
+                high=mid-1
+            else:
+                low=mid+1
+        return lb
+    def Upperbound(self,nums,target):
+        ub=len(nums)
+        low=0
+        high=len(nums)-1
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]>target:
+                ub=mid
+                high=mid-1
+            else:
+                low=mid+1
+        return ub
 
-        return [startPos,endPos]
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        lb=self.lowerbound(nums,target)
+        if lb == -1 or nums[lb]!=target:
+            return [-1,-1]
+        ub=self.Upperbound(nums,target)
+        return [lb,ub-1]
 
                 
             
